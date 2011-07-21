e871f9c0f4f652adff51e725d660dd9c489c8f9c
Daniel <daniel@cloudspace.com>
2011-07-21 10:38:25 -0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FrenchMessenger::Application.config.secret_token = 'e6f27e2f673a53e8876e05dcaa6d53a01207b22edfa26bd7c68ddb2236c4d5d7467ecddd412d6340d3ce9789c18e184725fd64718c5e36db9026e48ae2e87469'
